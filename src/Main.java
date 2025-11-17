8b0be02020bee143fb993e55e49f86fd97776636
JessFreak <maliy.olexander@lll.kpi.ua>
2025-11-17 05:01:48 +0200

    converted Main.java into a compact source file

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
-import universal.*;
+import universal.Dispose;
+import universal.Distribution;
+import universal.Model;
+import universal.Route;
 
-public class Main {
-    public static void main(String[] args) {
-        ShipGenerator generator = new ShipGenerator(
-                "Ship Generator",
-                1.25,
-                0.5,
-                1.5
-        );
-        generator.setDistribution(Distribution.EXPONENTIAL);
-        Harbor harbor = new Harbor("Harbor Process");
-        Dispose exit = new Dispose("Exit Dispose");
+void main() {
+    ShipGenerator generator = new ShipGenerator(
+            "Ship Generator",
+            1.25,
+            0.5,
+            1.5
+    );
+    generator.setDistribution(Distribution.EXPONENTIAL);
+    Harbor harbor = new Harbor("Harbor Process");
+    Dispose exit = new Dispose("Exit Dispose");
 
-        generator.addRoutes(new Route(harbor));
-        harbor.addRoutes(new Route(exit));
+    generator.addRoutes(new Route(harbor));
+    harbor.addRoutes(new Route(exit));
 
-        Model model = new Model(generator, harbor, exit);
-        double simulationTime = 100_000_00.0;
-        System.out.println("--- Starting simulation for " + simulationTime + " days ---");
+    Model model = new Model(generator, harbor, exit);
+    double simulationTime = 100_000_00.0;
+    IO.println("--- Starting simulation for " + simulationTime + " days ---");
 
-        model.simulate(simulationTime);
-    }
+    model.simulate(simulationTime);
 }
